ed17e9a93a2932655bdcf683c6ba48f5fcd14a37
Aleksandar9999 <aleksandar1999@uns.ac.rs>
2021-04-04 23:16:33 +0200

    problem sa splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:student_connection/pages/loginpage.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +15,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: AnimatedSplashScreen(
+        splash: Image.asset("files/logoforweb.jpg"),
+        nextScreen: LoginPage(),
+        splashTransition: SplashTransition.scaleTransition,
+        duration: 500,
+      ),
     );
   }
 }
